1722a0635aaec6cb6b583558090f29601da8bec3
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2024-07-03 11:05:04 +0200

    created fil to access localstorage

---
+++ app/utils/Storage.js
@@ -0,0 +1,17 @@
+const storeData = async (key, value) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem(key, jsonValue);
+  } catch (e) {
+    throw new Error("Error storing data");
+  }
+};
+
+const getData = async (key) => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(key);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    throw new Error("Error getting data");
+  }
+};
