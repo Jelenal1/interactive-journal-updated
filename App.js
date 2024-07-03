9b14bb351c490c81dea2b0486e509d8b712b33e0
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2024-07-03 10:57:13 +0200

    set colorscheme to dark

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./app/screens/Home";
+import { useEffect } from "react";
+import { useColorScheme } from "nativewind";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const { colorScheme, setColorScheme } = useColorScheme();
+  useEffect(() => {
+    setColorScheme("dark");
+  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator
