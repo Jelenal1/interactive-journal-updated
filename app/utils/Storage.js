d2c20334edfe616ae30198b43236b0d3a929fa73
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2024-07-03 14:30:34 +0200

    added extra functions

--- app/utils/Storage.js
+++ app/utils/Storage.js
@@ -19,4 +19,77 @@ const getData = async (key) => {
   }
 };
 
-export { storeData, getData };
+const addItem = async (key, listId, item) => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(key);
+    let lists = jsonValue != null ? JSON.parse(jsonValue) : null;
+    if (!lists) {
+      return;
+    }
+    let list = lists.find((list) => list.id === listId);
+    const newItem = {
+      id: list.items.length + 1,
+      name: item,
+      imageUri: "",
+      done: false,
+    };
+    list.items.push(newItem);
+    let newLists = lists.filter((l) => l.id !== listId);
+    newLists.push(list);
+    await AsyncStorage.removeItem(key);
+    console.log(newLists);
+    await AsyncStorage.setItem(key, JSON.stringify(newLists));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const updateItem = async (key, listId, itemId, newItem) => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(key);
+    let lists = jsonValue != null ? JSON.parse(jsonValue) : null;
+    if (!lists) {
+      return;
+    }
+    let list = lists.find((list) => list.id === listId);
+    let item = list.items.find((item) => item.id === itemId);
+    item.name = newItem.name;
+    item.imageUri = newItem.imageUri;
+    item.done = newItem.done;
+    let newLists = lists.filter((l) => l.id !== listId);
+    newLists.push(list);
+    await AsyncStorage.removeItem(key);
+    console.log(newLists[0].items);
+    await AsyncStorage.setItem(key, JSON.stringify(newLists));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const removeItem = async (key, listId, itemId) => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(key);
+    let lists = jsonValue != null ? JSON.parse(jsonValue) : null;
+    if (!lists) {
+      return;
+    }
+    console.log(lists);
+    let list = lists.find((list) => list.id === listId);
+    let newItems = list.items.filter((item) => item.id !== itemId);
+    let newlist = {
+      id: list.id,
+      name: list.name,
+      description: list.description,
+      items: newItems,
+    };
+    let newLists = lists.filter((l) => l.id !== listId);
+    newLists.push(newlist);
+    console.log(newLists[0].items);
+    await AsyncStorage.removeItem(key);
+    await AsyncStorage.setItem(key, JSON.stringify(newLists));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export { storeData, getData, addItem, updateItem, removeItem };
