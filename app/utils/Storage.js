b52b9e71980c27c8977fdc6e78112dc45ef6bcc4
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2024-07-04 15:48:50 +0200

    updated all

--- app/utils/Storage.js
+++ app/utils/Storage.js
@@ -1,6 +1,5 @@
-const AsyncStorage =
-  require("@react-native-async-storage/async-storage").default;
-const Storage = require("react-native-storage");
+import Storage from "react-native-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const storage = new Storage({
   size: 1000,
